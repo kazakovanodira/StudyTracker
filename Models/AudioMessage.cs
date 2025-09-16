f78157cc596b4297a1f6feb3a2d7cbe3728c2d3f
kazakovanodira <kazakovanodira04@gmail.com>
2025-09-16 09:37:27 -0400

    Add AudioMessage model

---
+++ Models/AudioMessage.cs
@@ -0,0 +1,16 @@
+namespace StudyTracker.Models;
+
+public class AudioMessage
+{
+    public Guid AudioId { get; set; }
+    public string FilePath { get; set; } = "";
+    public Category Category { get; set; }
+    public Guid OwnerId { get; set; }
+}
+
+public enum Category
+{
+    Little, 
+    Good, 
+    Exceptional
+}
--- Models/StudyLog.cs
+++ Models/StudyLog.cs
@@ -2,7 +2,7 @@ namespace StudyTracker.Models;
 
 public class StudyLog
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public DateTime Date { get; set; }
     public double Hours { get; set; }
 }
