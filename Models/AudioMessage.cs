5a6bb6beec6542f55efc606afe624f2218f9ff52
kazakovanodira <kazakovanodira04@gmail.com>
2025-09-16 12:24:59 -0400

    Add fields to ensure relationship to the owner in AudioMessage.cs

--- Models/AudioMessage.cs
+++ Models/AudioMessage.cs
@@ -5,7 +5,9 @@ public class AudioMessage
     public Guid AudioId { get; set; }
     public string FilePath { get; set; } = "";
     public Category Category { get; set; }
+
     public Guid OwnerId { get; set; }
+    public AudioOwner Owner { get; set; } 
 }
 
 public enum Category
